1a7dd3a4f4f2281f3e2be675573fb25c4f444714
TiagoFSDC <tiagofsdc@gmail.com>
2023-04-20 15:28:23 -0300

    Add project files.

---
+++ ProjAPIEF20042023/Models/Address.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.CodeAnalysis.Options;
+
+namespace ProjAPIEF20042023.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string? Street { get; set; }
+        public int? Number { get; set; }
+    }
+}
